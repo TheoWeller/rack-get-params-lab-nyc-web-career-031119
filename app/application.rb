ace5ba08ffbfb4c719c902900dd28641d79afe73
Theo Weller <weller.theodor@gmail.com>
2019-04-01 17:51:05 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -16,11 +16,12 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
 
-    elsif req.path.match(/cart/)
-      if @@cart.length == 0
-        resp.write "Your cart is empty."
       else
         @@cart.each {|item| resp.write "#{item}\n"}
+        
+    elsif req.path.match(/cart/)
+        if @@cart.length == 0
+          resp.write "Your cart is empty."
     elsif req.path.match(/add/)
       add_item = req.params["q"]
       @@items.each do |item|
